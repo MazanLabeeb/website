fd312e39ac07f006e6d603bbf466530b7b547171
Lahrasab Razman <s3neo36@gmail.com>
2022-12-28 14:42:44 +0500

    added files

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+require("./partials/header.php");
+?>
+        <!-- Page Header-->
+        <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>Networking & DevOps 101</h1>
+                            <span class="subheading">Blog for discussing new technologies in Networking and DevOps World </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Main Content-->
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="./posts/nat-pat-post.php">
+                            <h2 class="post-title">NAT / PAT the unsung heros of networking world</h2>
+                            <h3 class="post-subtitle">Get the lowdown on NAT and PAT and how they can help you connect to the internet.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Lahrasab Ramzan</a>
+                            on December 28, 2022
+                        </p>
+                    </div>
+                    <div class="post-preview">
+                        <a href="post.php">
+                            <h2 class="post-title">Topology 101 </h2>
+                            <h3 class="post-subtitle">Understanding the Different Types of Network Configurations</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Lahrasab Ramzan</a>
+                            on December 28, 2022
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="post.php">
+                            <h2 class="post-title">The Evolution of Devops: A Brief History</h2>
+                            <h3 class="post-subtitle">Trace the evolution of devops from its origins in the early 2000s to its current status as a leading approach to software devel...</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Lahrasab Ramzan</a>
+                            on tbd
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="/post.php">
+                            <h2 class="post-title">Subnetting and IPv6: Understanding the Transition</h2>
+                            <h3 class="post-subtitle">Subnetting and IPv6 are important concepts in networking that help organizations efficiently allocate and mana...</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Lahrasab Ramzan</a>
+                            on tbd
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Pager-->
+                    <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+                </div>
+            </div>
+        </div>
+
+<?php
+require("./partials/footer.php");
+?>
